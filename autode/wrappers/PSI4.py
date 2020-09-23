e516061aea3d70ebfa8d97be2d22c64a09e7f2e7
dpregeljc <70568960+dpregeljc@users.noreply.github.com>
2020-09-23 10:10:40 +0100

    Update PSI4.py

--- autode/wrappers/PSI4.py
+++ autode/wrappers/PSI4.py
@@ -1,94 +1,131 @@
+import numpy as np
 from autode.wrappers.base import ElectronicStructureMethod
+from autode.utils import run_external_monitored
+from autode.atoms import Atom
+form autode.config import Config
 from autode.log import logger
+from autode.utils import work_in_tmp_dir
 
 
 class PSI4(ElectronicStructureMethod):
 
-    def generate_input(self, calc, molecule): # NOT FINISHED
+    def generate_input(self, calc, molecule): # NEW
 
         with open(calc.input.filename, 'w') as inp_file:
-            print(f'invoked by autodE\n', file=inp_file)
-            print(f'molecule  {molecule.name} ', '{')
-            # example:
-            #   O
-            #   H 1 0.96
-            #   H 1 0.96 2 104.5
+            print(f'# {calc.name}, invoked by autodE\n', file=inp_file)
+
+            print(f'molecule  {molecule.name} ', '{\n')
+            print(molecule.charge, molecule.mult, file=inp_file)
+
+            for atom in molecule.atoms:
+                x, y, z = atom.coord
+                print(f'{atom.label:<3} {x:^12.8f} {y:^12.8f} {z:^12.8f}',
+                      file=inp_file)
             print('}\n')
-            # example:
-            # set basis cc-pVDZ
-            # optimize('scf')
+            
+            print(f'set basis {calc_input.keywords.sp[1]}')
+            print(f'energy({calc_input.keywords.sp[0]})')
 
+        return None
 
-        raise NotImplementedError
-
-    # input and output files can have arbitrary extensions for PSI4, we will use .inp for input and .out for output
-    # PSI4 is invoked by: 'psi4 input_file.inp output_file.out'
     def get_input_filename(self,calc):
+        '''Input and output files can have arbitrary extensions for PSI4,
+        we will use .inp for input and .out for output. PSI4 is invoked by:
+        psi4 input_file.inp output_file.out'''
         return f'{calc.name}.inp'
 
     def get_output_filename(self,calc):
         return f'{calc.name}.out'
 
-    def execute(self,calc):
-        raise NotImplementedError
+    def execute(self,calc): # NEW
+
+        @work_in_tmp_dir(filenames_to_copy=calc.input.get_input_filenames(),
+                         kept_file_exts=('.inp', '.out', '.dat'))
+        def execute_psi4():
+            logger.info(f'Setting the number of OMP threads to {calc.n_cores}')
+            os.environ['OMP_NUM_THREADS'] = str(calc.n_cores)
+            run_external(params=[calc.method.path, calc.input.filename],
+                         output_filename=calc.output.filename)
+
+        execute_psi4()
+        raise None
 
     def calculation_terminated_normally(self,calc):
         for n_line, line in enumerate(reversed(calc.output.file_lines)):
-            if 'Psi4 exiting succesfully' in line or 'Buy a developer a beer!' in line:
+            if 'Psi4 exiting succesfully' in line:
                 logger.info('PSI4 exited successfully.')
                 return True
-            elif 'Psi4 encountered an error.' in line or 'Buy a developer more coffee!' in line:
+            elif 'Psi4 encountered an error.' in line:
                 logger.info('PSI4 encountered an error.')
                 return False
             elif n_line > 10:
-                # Message saying whether the job was successfull is usually in the last few lines in PSI4
-                logger.info('Could not determine whether PSI4 exited successfully.')
+                # Message saying whether the job was successfull is
+                # usually in the last few lines in PSI4.
+                logger.info('Do not know whether PSI4 exited successfully.')
                 return False
+         return False
 
     def get_energy(self,calc):
-        'Output is in Eh.'
+        '''Output is in Eh.'''
         for line in reversed(calc.output.file_lines):
             if '@' in line and 'Final Energy' in line:
                 return float(line.split()[-1])
-        logger.info('Could not find energy in psi4 output file.')
-        return NoCalculationOutput
+        logger.info('Could not find energy from psi4.')
+        return None
 
     def get_zero_point_energy(self,calc):
-        'frequency() has to be used in the psi4 input file in order for this function to work. output is in Eh.'
+        '''frequency() has to be used in the psi4 input file in order
+        for this function to work. Output is in Eh.'''
         for line in reversed(calc.output.file_lines):
             if 'Total ZPE' in line and 'Electronic energy' in line:
                 return float(line.split()[-2])
-        logger.info('Could not find zero point energy in the psi4 output file.')
-        return NoCalculationOutput
+        logger.info('Could not find zero point energy from psi4.')
+        return None
 
     def get_enthalpy(self,calc):
-        'frequency() has to be used in the psi4 input file in order for this function to work. Output is in Eh.'
+        '''frequency() has to be used in the psi4 input file in order
+        for this function to work. Output is in Eh.'''
         for line in reversed(calc.output.file_lines):
             if 'Total H' in line and 'Enthalpy' in line:
                 return float(line.split()[-2])
         logger.info('Could not find enthalpy in the psi4 output file.')
-        raise NoCalculationOutput
+        raise None
 
     def get_free_energy(self,calc):
-        'frequency() has to be used in the psi4 input file in order for this function to work. Output is in Eh.'
+        '''frequency() has to be used in the psi4 input file in order
+        for this function to work. Output is in Eh.'''
         for line in reversed(calc.output.file_lines):
             if 'Total G' in line and 'Free enthalpy' in line:
                 return float(line.split()[-2])
-        logger.info('Could not find Gibbs free energy in the psi4 output file.')
-        return NoCalculationOutput
+        logger.info('Could not find Gibbs free energy from psi4.')
+        return None
 
     def optimisation_converged(self,calc):
         for line in reversed(calc.output.file_lines):
             if 'Energy and wave function converged.' in line:
                 return True
-            # include a block to return False if a keyword is found that says energy did not converge - I do not know the keyword yet
+            # include a block to return False if a keyword is found that
+            # says energy did not converge - I do not know the keyword yet
         return False
 
     def optimisation_nearly_converged(self,calc):
         raise NotImplementedError
 
-    def get_imaginary_freqs(self,calc):
-        raise NotImplementedError
+    def get_imaginary_freqs(self,calc): # NEW
+        '''frequencies() needs to be used in the psi4 input file.'''
+        imaginary_frequencies = []
+
+        for line in calc.output.file_lines:
+            if 'post-proj' in line and 'i' in line and '[cm^-1]' in line:
+                imaginary_frequencies.append(line.split()[3])
+
+        if len(imaginary_frequencies) > 0:
+            logger.info(f'Found imaginary frequencies {imaginary_frequencies}')
+            return imaginary_frequencies
+
+        else:
+            logger.info('Could not find any imaginary frequencies.')
+            return None
 
     def get_normal_mode_displacements(self,calc,mode_number):
         raise NotImplementedError
@@ -96,27 +133,54 @@ class PSI4(ElectronicStructureMethod):
     def get_final_atoms(self,calc):
         atoms = []
         optimized = False
+        section = False
         for line in calc.output.file_lines:
             if 'Optimization is complete!' in line:
                 optimized = True
+                continue
 
-            elif optimized:
-                if len(line.split() == 4):
-                    try:
-                        atom_label, x, y, z = line.split()
-                        atoms.append(Atom(atom_label, x=float(x), y=float(y), z=float(z)))  # converting to float should raise an error if line does not contain coordinates
-                        return atoms
-                    except:
-                        pass
+            if optimized and 'Cartesian geometry  (in Angstrom)' in line:
+                section = True
+                continue
 
-        logger.info('Could not get the optimised geometry from the psi4 output file.')
+            if optimized and section and len(line.split()) == 4:
+                try:
+                    atom_label, x, y, z = line.split()
+                    atoms.append(Atom(atom_label, x=x, y=y, z=z))
+                except ValueError:
+                    pass
+
+            if optimised and 'Saving final (previous) structure.' in line:
+                return atoms
+
+        logger.info('Could not get the optimised geometry from psi4.')
         return AtomsNotFound
 
     def get_atomic_charges(self,calc):
         raise NotImplementedError
 
-    def get_gradients(self,calc):
-        raise NotImplementedError
+    def get_gradients(self,calc): # NEW
+        '''optimise() needs to be used in the psi4 input file.'''
+        gradients = []
+        gradient_section = False
+        line_in_section = 0
+
+        for line in calc.output.file_lines:
+            if '-Total Gradient:' in line:
+                gradient_section = True
+
+            if gradient_section and '*** tstop() called on' in line:
+                gradient_section = False
+                line_in_section = 0
+
+            if gradient_section:
+                line_in_section += 1
+
+            if line_in_section > 3 and line_in_section < (calc.n_atoms + 4):
+                x, y, z = line.split()[1:]
+                gradients.append(np.array([float(x), float(y), float(z)]))
+
+        return np.array(gradients)
 
     def __init__(self):
         super().__init__(name='psi4', path=Config.PSI4.path,
