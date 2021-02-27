6a23cea781870c13d224a8d46071d229c39d2789
Tom <tom.young@balliol.ox.ac.uk>
2021-02-27 15:30:31 +0000

    Correct test

--- tests/test_dimer_ts.py
+++ tests/test_dimer_ts.py
@@ -95,22 +95,28 @@ def test_dimer_2d(plot=False):
 def test_dimer_ts_sn2():
     """Test a 'dimer' transition state search for an SN2 reaction"""
 
-    dimer = Dimer(species_1=ade.Molecule('sn2_p1.xyz'),
-                  species_2=ade.Molecule('sn2_p2.xyz'),
+    dimer = Dimer(species_1=ade.Molecule('sn2_p1.xyz',
+                                         charge=-1, solvent_name='water'),
+                  species_2=ade.Molecule('sn2_p2.xyz',
+                                         charge=-1, solvent_name='water'),
                   method=ade.methods.XTB())
 
-    for _ in range(10):
-        dimer.optimise_rotation(phi_tol=0.08)
-        dimer.translate(init_step_size=0.1)
-        dimer.g1 = dimer._gradient(coordinates=dimer.x1)
+    dimer.optimise_rotation(phi_tol=0.08, max_iterations=50)
+
+    # Do  single translation step
+    dimer.translate()
+
+    # then optimise the translation
+    while dimer.iterations[-1].d > 1E-3:
+        dimer.translate()
 
     assert len(dimer.iterations) > 1
     assert dimer.iterations[-1].phi < 0.1
 
     final_point = ade.Molecule('sn2_p1.xyz')
-    final_point.coordinates = dimer.x1
+    final_point.coordinates = dimer.x0
     # TODO: remove this test print
     final_point.print_xyz_file(filename='tmp.xyz')
 
     dimer.iterations.print_xyz_file(species=dimer._species,
-                                    point=1)
+                                    point=0)
