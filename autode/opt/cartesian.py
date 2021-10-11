26f92170901d9eda018acf20985d7fc2d1748180
Unknown <ty13946@my.bristol.ac.uk>
2021-10-11 17:01:51 +0100

    Initial DIC implementation

---
+++ autode/opt/cartesian.py
@@ -0,0 +1,70 @@
+import numpy as np
+from time import time
+from autode.log import logger
+from autode.opt.coordinates import _OptCoordinates
+from autode.opt.dic import DIC
+
+
+class CartesianCoordinates(_OptCoordinates):
+    """Flat Cartesian coordinates shape = (3 × n_atoms, )"""
+
+    def __repr__(self):
+        return f'Cartesian Coordinates({np.ndarray.__str__(self)} {self.units.name})'
+
+    def __new__(cls, input_array, units='Å') -> 'CartesianCoordinates':
+        """New instance of these coordinates"""
+
+        return super().__new__(cls, input_array.flatten(), units=units)
+
+    def __array_finalize__(self, obj) -> None:
+        """See https://numpy.org/doc/stable/user/basics.subclassing.html"""
+
+        if obj is None:
+            return
+
+        for attr in ('B', '_B_T_inv'):
+            self.__dict__[attr] = getattr(obj, attr, None)
+
+        return super().__array_finalize__(obj)
+
+    @property
+    def g(self) -> np.ndarray:
+        """
+        Gradient: {dE/dx_i}
+
+        Returns:
+            (np.ndarray): Gradient
+        """
+        return self._g
+
+    @property
+    def h(self) -> np.ndarray:
+        """Hessian: {d^2E/dx_idx_j^2}
+
+        Returns:
+            (np.ndarray): Hessian
+        """
+        return self._h
+
+    def to(self, value: str) -> _OptCoordinates:
+        """
+        Allow for the transformation between cartesian and internal coordinates
+        e.g. delocalised internal coordinates
+
+        Arguments:
+            value (str):
+
+        Retuns:
+            (autode.opt.coordinates._OptCoordinates):
+        """
+
+        if value in ('dic', 'delocalised internal coordinates'):
+            return DIC.from_cartesian(self)
+
+        # ----------- Implement other internals here ------------
+
+        elif any(value in unit.aliases for unit in self.implemented_units):
+            return CartesianCoordinates(super().to(units=value).flatten(),
+                                        units=value)
+        else:
+            raise ValueError('Cannot convert')
---
+++ autode/opt/coordinates.py
@@ -0,0 +1,56 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from autode.units import (ang, nm, pm, m)
+from autode.values import ValueArray
+
+
+class _OptCoordinates(ValueArray, ABC):
+    """Coordinates used to perform optimisations"""
+
+    implemented_units = [ang, nm, pm, m]
+
+    @abstractmethod
+    def __repr__(self):
+        """Representation of these coordinates"""
+
+    def __new__(cls, input_array, units) -> '_OptCoordinates':
+        """New instance of these coordinates"""
+
+        arr = super().__new__(cls, input_array, units)
+        arr._g, arr._h = None, None
+        arr.B = None               # Wilson B matrix
+        arr.B_T_inv = None         # Generalised inverse of B
+
+        return arr
+
+    def __array_finalize__(self, obj: '_OptCoordinates') -> None:
+        """See https://numpy.org/doc/stable/user/basics.subclassing.html"""
+
+        for attr in ('units', '_g', '_h', 'B', 'B_T_inv'):
+            self.__dict__[attr] = getattr(obj, attr, None)
+
+        return None
+
+    @property
+    def g(self) -> np.ndarray:
+        """Gradient of the energy"""
+        return self._g
+
+    @property
+    def h(self) -> np.ndarray:
+        """Second derivatives of the energy"""
+        return self._h
+
+    @g.setter
+    def g(self, value) -> np.ndarray:
+        """Set the gradient of the energy"""
+        self._g = value
+
+    @h.setter
+    def h(self, value) -> np.ndarray:
+        """Set the second derivatives of the energy"""
+        self._h = value
+
+    @abstractmethod
+    def to(self, *args, **kwargs):
+        """Transformation between these coordinates and another type"""
